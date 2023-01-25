cbe83ea78f92e61fd94856ea9a745bb0982be6c8
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-01-25 10:51:44 +0530

    feature: functionality added
    some basic styling and functionality added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log("Checking leap year!");
+let inputYear = document.querySelector("#input-year");
+let outputYear = document.querySelector("#output-year");
+let btnCheck = document.querySelector("#btn-check");
+
+function leapYear(age) {
+    let ageNum = Number.parseInt(age);
+    if((ageNum % 400 == 0) || (ageNum % 100 != 0 && ageNum % 4 == 0)){
+        return ("It's a leap year.");
+    }else if(age != ageNum){
+        return ("Error: Invalid input");
+    }
+    else{
+        return ("It's not a leap year.");
+    }
+}
+
+
+function clickEventHandler(){
+    console.log("clicked!");
+    outputYear.innerText = leapYear(inputYear.value);
+}
+
+btnCheck.addEventListener("click", clickEventHandler);
